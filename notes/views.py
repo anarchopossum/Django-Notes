d64459207a70e69b2a9c0bae82e95fbc1d861694
anarchopossum <jasmine.sanjuan01@student.csulb.edu>
2023-05-23 16:21:35 -0700

    changed the function based views to class based ones on Notes

--- notes/views.py
+++ notes/views.py
@@ -1,28 +1,19 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 
 from .models import Notes
 
 
-# Create your views here.
+class NoteListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
 
 
-def list(request):
-    # grab all items from the database this database was generated in Models
-    # then after the migration commands passed
-    # generated database configs are in /migrations/
-    # then once finalized through the second migration command it's finally added to the DB
-    # From here all_notes grabs from the database.
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+# No need to add 404 information for classes
 
-
-# This next def will help display the contents of the notes
-# to see the notes refer to the GitHub comments on when this section was generated
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note does not exist.")
-    return render(request, 'notes/notes_detail.html', {'note': note})
