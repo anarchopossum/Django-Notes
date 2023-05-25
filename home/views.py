9d3942d9ff59e9c214824e193ee024317277d29c
anarchopossum <jasmine.sanjuan01@student.csulb.edu>
2023-05-25 16:51:11 -0700

    Proper Registration is now avalible.
    New users can now login to our site and create, Edit, Delete and read their notes. Also added more buttons to make the site easy to use

--- home/views.py
+++ home/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from datetime import datetime
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+# this is used in the get method in SignupView
+from django.shortcuts import redirect
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = 'smart/notes'
+
+    # the guide said they were seeing other users notes. this is what they implemented for this
+    # although you weren't having that issue with your code.
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args,**kwargs)
+
 
 
 class LogoutInterfaceView(LogoutView):
