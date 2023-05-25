cf64810498a5f457e87dc437c9666c2dc6014a04
anarchopossum <jasmine.sanjuan01@student.csulb.edu>
2023-05-25 12:28:34 -0700

    You can now navigate pages better, update notes

--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('notes', views.NoteListView.as_view(), name='notes.list'),
+    # <int:pk> means the numerical value for the note
     path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
+    path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name="notes.update"),
     path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 
 from .models import Notes
+from .forms import NotesForm
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    # this comes from forms.py
+    form_class = NotesForm
+    # Once the Note is created, it will redirect to the notes list.
+    success_url = '/smart/notes'
 
 
 class NotesCreateView(CreateView):
     model = Notes
-    # These will be the fields that will be used to input a new note
-    fields = ['title', 'text']
+    # this comes from forms.py
+    form_class = NotesForm
     # Once the Note is created, it will redirect to the notes list.
     success_url = '/smart/notes'
 
