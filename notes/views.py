d2f8e7a64d0c77a60d1a68308caa86bb0b97c17c
anarchopossum <jasmine.sanjuan01@student.csulb.edu>
2023-05-24 21:29:49 -0700

    Added a Way to implement New notes from a webpage.

--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 
 
 from .models import Notes
+
+
+class NotesCreateView(CreateView):
+    model = Notes
+    # These will be the fields that will be used to input a new note
+    fields = ['title', 'text']
+    # Once the Note is created, it will redirect to the notes list.
+    success_url = '/smart/notes'
 
 
 class NoteListView(ListView):
